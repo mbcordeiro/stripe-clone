feab4da69b9322cdeb2afbd9169defadac3a409e
Matheus Cordeiro <matheusdebarroscordeiro@gmail.com>
2020-09-04 13:01:22 -0300

    Option component

--- src/components/Dropdown/Option.js
+++ src/components/Dropdown/Option.js
@@ -1,5 +1,90 @@
-import React from "react";
+import React, { useRef, useState, useContext, useEffect } from "react";
+import { motion } from "framer-motion";
 
-export function DropdownOption({ name, content: Content }) {
-  return <button>{name}</button>;
+import { useDimensions } from "./dimensions";
+import { Context } from "./Provider";
+
+let lastOption = 0;
+
+export function DropdownOption({ name, content: Content, backgroundHeigth }) {
+  const idRef = useRef(++lastOption);
+  const id = idRef.current;
+
+  const [optionsHook, optionDimensions] = useDimensions();
+  const [registered, setRegistered] = useState(false);
+
+  const {
+    registerOption,
+    updateOptionProps,
+    deleteOptionById,
+    setTargetId,
+    targetId,
+  } = useContext(Context);
+
+  useEffect(() => {
+    if (!registered && optionDimensions) {
+      const WrappedContent = () => {
+        const contentRef = useRef();
+
+        useEffect(() => {
+          const contentDimensions = contentRef.current.getBoundingClientRect();
+          updateOptionProps(id, { contentDimensions });
+        }, []);
+
+        return (
+          <div ref={contentRef}>
+            <Content />
+          </div>
+        );
+      };
+
+      registerOption({
+        id,
+        optionDimensions,
+        optionCenterX: optionDimensions.x + optionDimensions.width / 2,
+        WrappedContent,
+        backgroundHeight,
+      });
+
+      setRegistered(true);
+    } else if (registered && optionDimensions) {
+      updateOptionProps(id, {
+        optionDimensions,
+        optionCenterX: optionDimensions.x + optionDimensions.width / 2,
+      });
+    }
+  }, [
+    registerOption,
+    id,
+    registered,
+    optionDimensions,
+    updateOptionProps,
+    deleteOptionById,
+    backgroundHeight,
+  ]);
+
+  const handleOpen = () => setTargetId(id);
+  const handleClose = () => setTargetId(null);
+  const handleTouch = () => (window.isMobile = true);
+
+  const handleClick = (e) => {
+    e.preventDefault();
+
+    return targetId === id ? handleClose() : handleOpen();
+  };
+
+  return (
+    <motion.button
+      className="dropdown-option"
+      ref={optionHook}
+      onMouseDown={handleClick}
+      onHoverStart={() => !window.isMobile && handleOpen()}
+      onHoverEnd={() => !window.isMobile && handleClose()}
+      onTouchStart={handleTouch}
+      onFocus={handleOpen}
+      onBlur={handleClose}
+    >
+      {name}
+    </motion.button>
+  );
 }
